b7db9a7801015441796922440a1e5a4ec4920705
janvi33 <janvibhalala02@gmail.com>
2023-12-07 16:19:48 -0500

    final

--- atm.py
+++ atm.py
@@ -1,34 +1,186 @@
-import sys
+# import socket
+# from cryptography.hazmat.primitives import serialization
+# from cryptography.hazmat.primitives.asymmetric import padding
+# from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.backends import default_backend
+# from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+# import getpass
+# import sys
+# import secrets
+
+# SYMMETRIC_KEY = secrets.token_bytes(32)  # Use a 256-bit key for AES
+
+# def load_public_key(filename):
+#     return serialization.load_pem_public_key(open(filename, "rb").read(), backend=default_backend())
+
+# def encrypt_txt(message, key):
+#     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend()).encryptor()
+#     padded_message = message + b'\0' * (16 - len(message) % 16)
+#     return cipher.update(padded_message) + cipher.finalize()
+
+# def encrypt_with_public_key(public_key, data):
+#     ciphertext = public_key.encrypt(
+#         data,
+#         padding.OAEP(
+#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
+#             algorithm=hashes.SHA256(),
+#             label=None
+#         )
+#     )
+#     return ciphertext
+
+# def send_encrypted_credentials(client_socket, public_key, user_id, password):
+#     encrypted_key = encrypt_with_public_key(public_key, SYMMETRIC_KEY)
+#     client_socket.sendall(encrypted_key)
+
+#     cipher = Cipher(algorithms.AES(SYMMETRIC_KEY), modes.ECB(), backend=default_backend()).encryptor()
+#     encrypted_credentials = encrypt_txt(f"ID: {user_id} Password: {password}".encode(), SYMMETRIC_KEY)
+#     client_socket.sendall(encrypted_credentials)
+
+#     response = client_socket.recv(1024).decode()
+#     return response
+
+# def send_request(client_socket, public_key, request):
+#     cipher = Cipher(algorithms.AES(SYMMETRIC_KEY), modes.ECB(), backend=default_backend()).encryptor()
+#     encrypted_request = encrypt_txt(request.encode(), SYMMETRIC_KEY)
+#     client_socket.sendall(encrypted_request)
+
+#     response = client_socket.recv(1024).decode()
+#     return response
+
+# def main_menu():
+#     print("Please select one of the following actions (enter 1, 2, or 3):")
+#     print("1. Transfer money")
+#     print("2. Check account balance")
+#     print("3. Exit")
+
+# def transfer_money(client_socket, public_key, user_id):
+#     while True:
+#         print("Please select an account (enter 1 or 2):")
+#         print("1. Savings")
+#         print("2. Checking")
+#         account_choice = input("Select account: ")
+#         if account_choice not in ["1", "2"]:
+#             print("Incorrect input. Please select either the savings or checking account.")
+#             continue
+#         recipient_id = input("Enter the recipient's ID: ")
+#         if recipient_id == user_id:
+#             print("Recipient ID cannot be the same as your ID. Please try again.")
+#             continue
+#         amount = input("Enter the amount to be transferred: ")
+#         try:
+#             amount = float(amount)
+#         except ValueError:
+#             print("Invalid amount. Please enter a numeric value.")
+#             continue
+#         transfer_request = f"Transfer {account_choice} {recipient_id} {amount}"
+#         response = send_request(client_socket, public_key, transfer_request)
+#         print(f"Server response: {response}")
+#         break
+
+# def generateKeys():
+#     public_key = serialization.load_pem_public_key(open("public_key.pem", "rb").read(), backend=default_backend())
+#     return public_key
+
+# def connect_to_bank_server(host, port, public_key_path):
+#     public_key = generateKeys()
+
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+#         client_socket.connect((host, port))
+#         print(f"Connected to bank server at {host}:{port}")
+
+#         while True:  
+#             user_id = input("Enter your ID: ")
+#             password = getpass.getpass("Enter your password: ")
+#             response = send_encrypted_credentials(client_socket, public_key, user_id, password)
+
+#             if response == "ID and password are correct":
+#                 while True:
+#                     main_menu()
+#                     choice = input("Your choice: ")
+
+#                     if choice == "1":
+#                         transfer_money(client_socket, public_key, user_id)
+#                     elif choice == "2":
+#                         balance_info = send_request(client_socket, public_key, "2")
+#                         print(balance_info)
+#                     elif choice == "3":
+#                         print("Exiting.")
+#                         break
+#                     else:
+#                         print("Invalid choice. Please try again.")
+#                 break 
+#             else:
+#                 print("ID or password is incorrect. Please try again.")
+#                 retry = input("Do you want to try again? (yes/no): ")
+#                 if retry.lower() != 'yes':
+#                     print("Exiting.")
+#                     break
+
+# public_key_path = 'public_key.pem'
+# if len(sys.argv) < 3:
+#     print("Usage: python client.py <host> <port number>")
+# else:
+#     host = sys.argv[1]
+#     port = int(sys.argv[2])
+#     if host == "localhost" or not host:
+#         print("The host name should not be localhost or empty")
+#         sys.exit(1)
+
+#     if not (1024 <= port <= 65535):
+#         print("The port number should be a user-defined number between 1024 and 65535")
+#         sys.exit(1)
+
+#     connect_to_bank_server(host, port, public_key_path)
+
+
+
 import socket
+import sys
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
 import getpass
+from cryptography.hazmat.primitives import padding as sym_padding  # New import for symmetric padding
+
+
 
 def load_public_key(filename):
     with open(filename, "rb") as key_file:
         return serialization.load_pem_public_key(key_file.read())
 
-def encrypt_with_public_key(public_key, message):
-    return public_key.encrypt(
-        message.encode(),
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
+def encrypt_with_aes_and_public_key(public_key, message):
+    symmetric_key = os.urandom(32)  # 256-bit key
+
+    padder = sym_padding.PKCS7(128).padder()
+    padded_message = padder.update(message.encode()) + padder.finalize()
+
+    # Encrypt the message with AES
+    aesCipher = Cipher(algorithms.AES(symmetric_key), modes.ECB())
+    encryptor = aesCipher.encryptor()
+    encrypted_message = encryptor.update(padded_message) + encryptor.finalize()
+
+    # Encrypt the symmetric key with the public key
+    encrypted_symmetric_key = public_key.encrypt(
+        symmetric_key,
+        padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
     )
+
+    return encrypted_symmetric_key, encrypted_message
+
 
 def send_encrypted_credentials(client_socket, public_key, user_id, password):
     credentials = f"ID: {user_id} Password: {password}"
-    encrypted_credentials = encrypt_with_public_key(public_key, credentials)
+    encrypted_symmetric_key, encrypted_credentials = encrypt_with_aes_and_public_key(public_key, credentials)
+    client_socket.sendall(encrypted_symmetric_key)
     client_socket.sendall(encrypted_credentials)
     response = client_socket.recv(1024).decode()
     return response
 
-def send_request(client_socket, public_key, request):
-    encrypted_request = encrypt_with_public_key(public_key, request)
-    client_socket.sendall(encrypted_request)
+def send_request(client_socket, request):
+    client_socket.sendall(request.encode())
     response = client_socket.recv(1024).decode()
     return response
 
@@ -38,7 +190,7 @@ def main_menu():
     print("2. Check account balance")
     print("3. Exit")
 
-def transfer_money(client_socket, public_key, user_id):
+def transfer_money(client_socket, user_id):
     while True:
         print("Please select an account (enter 1 or 2):")
         print("1. Savings")
@@ -58,7 +210,7 @@ def transfer_money(client_socket, public_key, user_id):
             print("Invalid amount. Please enter a numeric value.")
             continue
         transfer_request = f"Transfer {account_choice} {recipient_id} {amount}"
-        response = send_request(client_socket, public_key, transfer_request)
+        response = send_request(client_socket, transfer_request)
         print(f"Server response: {response}")
         break
 
@@ -72,7 +224,7 @@ def connect_to_bank_server(host, port, public_key_path):
         server_public_key_data = client_socket.recv(1024)
         server_public_key = serialization.load_pem_public_key(server_public_key_data)
 
-        while True:  # Keep asking for ID and password until correct or exited
+        while True:
             user_id = input("Enter your ID: ")
             password = getpass.getpass("Enter your password: ")
             response = send_encrypted_credentials(client_socket, server_public_key, user_id, password)
@@ -81,32 +233,34 @@ def connect_to_bank_server(host, port, public_key_path):
                 while True:
                     main_menu()
                     choice = input("Your choice: ")
-
                     if choice == "1":
-                        transfer_money(client_socket, server_public_key, user_id)
+                        transfer_money(client_socket, user_id)
                     elif choice == "2":
-                        balance_info = send_request(client_socket, server_public_key, "2")
+                        balance_info = send_request(client_socket, "2")
                         print(balance_info)
                     elif choice == "3":
-                        print("Exiting.")
                         break
                     else:
                         print("Invalid choice. Please try again.")
-                break  # Break out of the outer loop if logged in successfully
+                break
             else:
                 print("ID or password is incorrect. Please try again.")
                 retry = input("Do you want to try again? (yes/no): ")
                 if retry.lower() != 'yes':
-                    print("Exiting.")
                     break
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script_name.py <host> <port>")
-        sys.exit(1)
-
+public_key_path = 'public_key.pem'
+if len(sys.argv) < 3:
+    print("Usage: python client.py <host> <port number>")
+else:
     host = sys.argv[1]
     port = int(sys.argv[2])
+    if host == "localhost" or not host:
+        print("The host name should not be localhost or empty")
+        sys.exit(1)
 
-    public_key_path = 'public_key.pem'  # Replace with your file path
-    connect_to_bank_server(host, port, public_key_path)
+    if not (1024 <= port <= 65535):
+        print("The port number should be a user-defined number between 1024 and 65535")
+        sys.exit(1)
+
+connect_to_bank_server(host, port, public_key_path)
--- bank.py
+++ bank.py
@@ -1,9 +1,204 @@
-import sys
+# import socket
+# import threading
+# from cryptography.hazmat.primitives import serialization
+# from cryptography.hazmat.primitives.asymmetric import padding
+# from cryptography.hazmat.primitives import hashes
+# import sys
+
+# def load_private_key(filename):
+#     with open(filename, "rb") as key_file:
+#         return serialization.load_pem_private_key(key_file.read(), password=None)
+
+# def load_public_key(filename):
+#     with open(filename, "rb") as key_file:
+#         return serialization.load_pem_public_key(key_file.read())
+
+# def decrypt_with_private_key(private_key, encrypted_message):
+#     return private_key.decrypt(
+#         encrypted_message,
+#         padding.OAEP(
+#             mgf=padding.MGF1(algorithm=hashes.SHA256()),
+#             algorithm=hashes.SHA256(),
+#             label=None
+#         )
+#     )
+
+# def validate_credentials(user_id, password, filename):
+#     with open(filename, 'r') as file:
+#         for line in file:
+#             line = line.strip()
+#             if not line:
+#                 continue
+#             parts = line.split(' ')
+#             if len(parts) != 2:
+#                 continue
+#             stored_id, stored_password = parts
+#             if user_id == stored_id and password == stored_password:
+#                 return True
+#         return False
+
+# def update_balances(balance_file, sender_id, recipient_id, amount, account_type):
+    
+#     with open(balance_file, 'r') as file:
+#         lines = file.readlines()
+
+#     balances = {}
+#     for line in lines:
+#         user_id, savings, checking = line.strip().split(' ')
+#         balances[user_id] = {'savings': float(savings), 'checking': float(checking)}
+
+#     if recipient_id not in balances or sender_id not in balances:
+#         return "The recipient's ID does not exist"
+
+#     if account_type == '1':
+#         account_key = 'savings'
+#     elif account_type == '2':
+#         account_key = 'checking'
+#     else:
+#         return "Invalid account type"
+
+#     if balances[sender_id][account_key] < amount:
+#         return "Your account does not have enough funds"
+
+#     balances[sender_id][account_key] -= amount
+#     balances[recipient_id][account_key] += amount
+
+#     with open(balance_file, 'w') as file:
+#         for user_id, balance in balances.items():
+#             file.write(f"{user_id} {balance['savings']} {balance['checking']}\n")
+
+#     return "Your transaction is successful"
+
+# def fetch_account_balances(balance_file, user_id):
+#     try:
+#         with open(balance_file, 'r') as file:
+#             for line in file:
+#                 parts = line.strip().split(' ')
+#                 if parts[0] == user_id:
+#                     savings_balance = parts[1]
+#                     checking_balance = parts[2]
+#                     return f"Your savings account balance: {savings_balance}\nYour checking account balance: {checking_balance}"
+#         return "User account not found."
+#     except Exception as e:
+#         return f"Error reading balance file: {e}"
+
+# def handle_client_connection(client_socket, private_key, public_key_path, credentials_file, balance_file):
+#     try:
+#         authenticated = False
+#         user_id = None
+
+#         with open(public_key_path, "rb") as public_key_file:
+#             public_key_bytes = public_key_file.read()
+#             client_socket.sendall(public_key_bytes)
+
+#         public_key = load_public_key(public_key_path)
+
+#         while True:
+#             public_key_bytes = client_socket.recv(1024)
+#             if not public_key_bytes:
+#                 break
+            
+#             symmetric_key = decrypt_with_private_key(private_key, client_socket.recv(1024))
+#             request = decrypt_with_private_key(symmetric_key, client_socket.recv(1024)).decode()
+
+#             if not authenticated:
+#                 if 'ID: ' in request and ' Password: ' in request:
+#                     user_id, password = request.split('ID: ')[1].split(' Password: ')
+#                     authenticated = validate_credentials(user_id, password, credentials_file)
+#                     response_message = "ID and password are correct" if authenticated else "ID or password is incorrect"
+#                 else:
+#                     response_message = "Invalid login format"
+#             else:
+#                 if request.startswith("Transfer"):
+#                     _, account_choice, recipient_id, amount = request.split()
+#                     response_message = update_balances(balance_file, user_id, recipient_id, float(amount), account_choice)
+#                 elif request == "2":
+#                     response_message = fetch_account_balances(balance_file, user_id)
+#                 else:
+#                     response_message = "Invalid request format"
+
+#             encrypted_response = public_key.encrypt(
+#                 response_message.encode(),
+#                 padding.OAEP(
+#                     mgf=padding.MGF1(algorithm=hashes.SHA256()),
+#                     algorithm=hashes.SHA256(),
+#                     label=None
+#                 )
+#             )
+
+#             client_socket.sendall(encrypted_response)
+#             print(f"Sent response: {response_message}")
+
+#     except Exception as e:
+#         print(f"An error occurred while handling client request: {e}")
+#     finally:
+#         client_socket.close()
+
+# def start_bank_server(port, private_key_path, public_key_path, credentials_file, balance_file):
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind(('', port))
+#     server_socket.listen(5)
+#     print(f"Bank server is listening on {port}")
+
+#     private_key = load_private_key(private_key_path)
+
+#     try:
+#         while True:
+#             client_sock, address = server_socket.accept()
+#             print(f"Accepted connection from {address}")
+
+#             with open(public_key_path, "rb") as public_key_file:
+#                 public_key_bytes = public_key_file.read()
+#                 client_sock.sendall(public_key_bytes)
+
+#             client_handler = threading.Thread(
+#                 target=handle_client_connection,
+#                 args=(client_sock, private_key, public_key_path, credentials_file, balance_file)
+#             )
+#             client_handler.start()
+#     finally:
+#         server_socket.close()
+
+# # Start the server
+# private_key_path = 'private_key.pem'
+# public_key_path = 'public_key.pem'
+# credentials_file = 'password'
+# balance_file = 'balance'
+# if len(sys.argv) < 2:
+#     print('Usage: python server.py <port number>')
+#     sys.exit(1)
+
+# port = int(sys.argv[1])
+# if not (1024 <= port <= 65535):
+#     print("The port number should be a user-defined number between 1024 and 65535")
+#     sys.exit(1)
+
+# start_bank_server(port, private_key_path, public_key_path, credentials_file, balance_file)
+
 import socket
 import threading
+import sys
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding as sym_padding
+
+def decrypt_with_private_key_and_aes(private_key, encrypted_symmetric_key, encrypted_message):
+    symmetric_key = private_key.decrypt(
+        encrypted_symmetric_key,
+        padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+    )
+
+    aesCipher = Cipher(algorithms.AES(symmetric_key), modes.ECB())
+    decryptor = aesCipher.decryptor()
+    decrypted_padded_message = decryptor.update(encrypted_message) + decryptor.finalize()
+
+    unpadder = sym_padding.PKCS7(128).unpadder()
+    decrypted_message = unpadder.update(decrypted_padded_message) + unpadder.finalize()
+
+    # The following line returns a string, so no need to call decode() on it
+    return decrypted_message
 
 def load_private_key(filename):
     with open(filename, "rb") as key_file:
@@ -13,27 +208,19 @@ def load_public_key(filename):
     with open(filename, "rb") as key_file:
         return serialization.load_pem_public_key(key_file.read())
 
-def decrypt_with_private_key(private_key, encrypted_message):
-    return private_key.decrypt(
-        encrypted_message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
 
 def validate_credentials(user_id, password, filename):
-    with open(filename, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if not line: continue
-            parts = line.split(' ')
-            if len(parts) != 2: continue
-            stored_id, stored_password = parts
-            if user_id == stored_id and password == stored_password:
-                return True
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                stored_id, stored_password = line.strip().split(' ')
+                if user_id == stored_id and password == stored_password:
+                    return True
         return False
+    except Exception as e:
+        print(f"Error in validate_credentials: {e}")
+        return False
+
 
 def update_balances(balance_file, sender_id, recipient_id, amount, account_type):
     
@@ -83,29 +270,22 @@ def fetch_account_balances(balance_file, user_id):
     except Exception as e:
         return f"Error reading balance file: {e}"
 
+
+
 def handle_client_connection(client_socket, private_key, credentials_file, balance_file):
     try:
+        encrypted_symmetric_key = client_socket.recv(256)
         authenticated = False
         user_id = None
 
         while True:
-            encrypted_request = client_socket.recv(1024)
-            if not encrypted_request:
-                print("No more data from client. Closing connection.")
-                break
+            if authenticated:
+                
+                request = client_socket.recv(1024).decode()
+                if not request:
+                    print("No more data from client. Closing connection.")
+                    break
 
-            request = decrypt_with_private_key(private_key, encrypted_request).decode()
-
-            if not authenticated:
-                # Extract and validate user credentials
-                if 'ID: ' in request and ' Password: ' in request:
-                    user_id, password = request.split('ID: ')[1].split(' Password: ')
-                    authenticated = validate_credentials(user_id, password, credentials_file)
-                    response_message = "ID and password are correct" if authenticated else "ID or password is incorrect"
-                else:
-                    response_message = "Invalid login format"
-            else:
-                # Handle authenticated requests
                 if request.startswith("Transfer"):
                     _, account_choice, recipient_id, amount = request.split()
                     response_message = update_balances(balance_file, user_id, recipient_id, float(amount), account_choice)
@@ -113,6 +293,16 @@ def handle_client_connection(client_socket, private_key, credentials_file, balan
                     response_message = fetch_account_balances(balance_file, user_id)
                 else:
                     response_message = "Invalid request format"
+            else:
+                encrypted_credentials = client_socket.recv(1024)
+                decrypted_credentials = decrypt_with_private_key_and_aes(private_key, encrypted_symmetric_key, encrypted_credentials).decode().rstrip("\0")
+                if 'ID: ' in decrypted_credentials and ' Password: ' in decrypted_credentials:
+                    user_id, password = decrypted_credentials.split('ID: ')[1].split(' Password: ')
+                    is_valid = validate_credentials(user_id, password, credentials_file)
+                    response_message = "ID and password are correct" if is_valid else "ID or password is incorrect"
+                    authenticated = is_valid
+                else:
+                    response_message = "Invalid login format"
 
             client_socket.sendall(response_message.encode())
             print(f"Sent response: {response_message}")
@@ -124,11 +314,11 @@ def handle_client_connection(client_socket, private_key, credentials_file, balan
 
 
 
-def start_bank_server(host, port, private_key_path, public_key_path, credentials_file, balance_file):
+def start_bank_server(port, private_key_path, public_key_path, credentials_file, balance_file):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
+    server_socket.bind(('', port))
     server_socket.listen(5)
-    print(f"Bank server is listening on {host}:{port}")
+    print(f"Bank server is listening on :{port}")
 
     private_key = load_private_key(private_key_path)
     public_key = load_public_key(public_key_path)
@@ -137,8 +327,6 @@ def start_bank_server(host, port, private_key_path, public_key_path, credentials
         while True:
             client_sock, address = server_socket.accept()
             print(f"Accepted connection from {address}")
-
-            # Send the public key to the client
             client_sock.sendall(public_key.public_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo
@@ -152,16 +340,26 @@ def start_bank_server(host, port, private_key_path, public_key_path, credentials
     finally:
         server_socket.close()
 
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script_name.py <host> <port>")
-        sys.exit(1)
 
-    host = sys.argv[1]
-    port = int(sys.argv[2])
+# Start the server
+private_key_path = 'private_key.pem'
+public_key_path = 'public_key.pem'
+credentials_file = 'password'
+balance_file = 'balance'
 
-    private_key_path = 'private_key.pem'
-    public_key_path = 'public_key.pem'
-    credentials_file = 'password'
-    balance_file = 'balance'
-    start_bank_server(host, port, private_key_path, public_key_path, credentials_file, balance_file)
+if len(sys.argv) < 2:
+    print('Usage: python server.py <port number>')
+    sys.exit(1)
+
+port = int(sys.argv[1])
+if not (1024 <= port <= 65535):
+    print("The port number should be a user-defined number between 1024 and 65535")
+    sys.exit(1)
+
+start_bank_server(port, private_key_path, public_key_path, credentials_file, balance_file)
+
+
+
+
+
+
